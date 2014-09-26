4b69aee57ed14d0a7c4d8be93f532bc91008a5fd
mgilmore208528 <mgilmore@stcharlessd.org>
2014-09-26 04:24:06 -0500

    Craps_Roller.py syntax fixes

--- Craps_Roller.py
+++ Craps_Roller.py
@@ -1,16 +1,16 @@
 # Craps Roller
 # Demonstrates random number generation
 # Original Author: Michael Dawson
-# Last Edited: Thorin Schmidt
+# Last Edited: Matt Gilmore
 # Date Edited: 9/26/2014
 
-impart random
+from random import randint
 
 # generate random numbers 1 - 6
-die1 = random.randint(1, 6) 
-die2 = random.randrange(6)
+die1 = randint(1, 6) 
+die2 = randint(1, 6)
 
-total = 'die1' + 'die2'
+total = (die1 + die2)
 
 print("You rolled a", die1, "and a", die2, "for a total of", total)
 
